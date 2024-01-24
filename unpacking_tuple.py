3118bdd9ffc0ebaf3053c36cf6a443237849463e
Rachit Alagundagi <alagundagirachit@gmail.com>
2024-01-25 00:15:39 +0530

    unpacking tuples from list

---
+++ unpacking_tuple.py
@@ -0,0 +1,11 @@
+#list can have tuple inside it 
+
+mylist = [(1,2),(3,4),(5,6),(7,8)]
+
+# this list has only 4 elements in it
+len(mylist)
+
+#unpacking tuple 
+for a,b in mylist:
+	print(a)
+	print(b)	
