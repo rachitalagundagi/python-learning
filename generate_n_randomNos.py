a2efdeedee6af032b8721e836bd12fe29d925b71
Rachit Alagundagi <alagundagirachit@gmail.com>
2024-01-31 14:43:15 +0530

    python code to generate random nos and nos with name

--- generate_n_randomNos.py
+++ generate_n_randomNos.py
@@ -12,19 +12,8 @@
 # 1231231235
 # 1231231230
 # 1231231239
+
 import random
-from random import shuffle
-
-
-# my_set = [0,1,2,3,4,5,6,7,8,9]
-
-# def shuff():
-# shuffle(my_set)
-# return my_set
-
-# print(shuff())
-# print(shuff())
-
 # a = random.randrange(1000000000, 9999999999)
 phoneNumbers = set()
 times = int(input("Tell how many numbers you want: "))
@@ -35,6 +24,22 @@ for i in range(times):
 
 print(phoneNumbers)
 
-# 99999999997
+#---------------------------------------------------------------------------------
+#Generate n nos of phone numbers with names using dictionaries
 
-# print(a)
+import random 
+my_dict = {}
+
+phoneNumbers = set()
+times = int(input("kitne chaiye? "))
+name = []
+
+for i in range(times):
+    a = random.randrange(1000000000, 9999999999)
+    name=input("hesar heli: ")
+    my_dict[name]=a
+
+    
+print(my_dict)    
+
+
