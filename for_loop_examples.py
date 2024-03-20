48dafad46a763f0a39e87e31168f622e9ee931d2
rachitalagundagi <alagundagirachit@gmail.com>
2024-03-21 00:40:29 +0530

    for loop examples

---
+++ for_loop_examples.py
@@ -0,0 +1,19 @@
+''' print 1- 10 nos in python'''
+
+for i in range(1,11):
+    print(i)
+
+#-----------------------------------------------------------------------
+
+
+'''print multiplication table of any integer i'''
+
+a = input("Please enter the no whose multiplication table you want.")
+print(f"multiplication table of {a} is: ")
+
+for i in range(1,11):
+    print(f"{int(a)} X {i} = {int(a)*i}" )
+
+
+#--------------------------------------------------------------------------------------------
+
