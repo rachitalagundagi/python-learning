8e1586a6c901ebf1d5c255f35c24aeb1c8b01e6c
rachitalagundagi <alagundagirachit@gmail.com>
2024-03-21 23:30:09 +0530

    code to understand modules

---
+++ modules_list.py
@@ -0,0 +1,15 @@
+#syntax: print(help("module_name"))
+
+# print(help("math"))
+
+# import math
+# print(math.e)
+
+a = 10 
+def area_of_square(side):
+    return side**2
+
+def area_of_circle(radius):
+    return 3.14*radius**2
+
+
