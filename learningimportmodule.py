1c0506e280b089caaa5b9eb84bbceb1acd632d28
rachitalagundagi <alagundagirachit@gmail.com>
2024-03-25 23:17:47 +0530

    code to understand import module

---
+++ learningimportmodule.py
@@ -0,0 +1,24 @@
+# import modules_list 
+# print(modules_list.a)
+
+# print(modules_list.area_of_square(3))
+# print(modules_list.area_of_circle(3))
+
+#--------------------------------------------------------------------------------------------
+
+
+# MODULES CAN BE IMPORTED AS SOME SMALL NAME SO YOU DONT HAVE TO TYPE BIG NAME EVERY TIME 
+# import modules_list as m 
+# print(m.a)
+
+# print(m.area_of_square(3))
+
+#---------------------------------------------------------------------------------------------
+
+
+# TO IMPORT JUST A SPECIFIC FUNCTION
+from modules_list import area_of_circle as c
+
+print(c(3))
+
+
