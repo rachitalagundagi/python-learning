b75b93833be14bc8c189a4e323a0b8f7e4f1ae58
rachitalagundagi <alagundagirachit@gmail.com>
2024-05-22 23:14:39 +0530

    all basics added to linkedlist file

--- LinkedListBasics.py
+++ LinkedListBasics.py
@@ -137,13 +137,33 @@ class LinkedList:
         self.length-=1
         return temp # WHY???? THIS STEP WHYYYYYY<>??????????????????????
 
+# my_linked_list = LinkedList(11)
+# my_linked_list.append(3)
+# my_linked_list.append(5)
+# my_linked_list.append(6)
+# my_linked_list.print_list()
+# print("after removing")
+# my_linked_list.remove(1)
+# my_linked_list.print_list()
+
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next= before
+            before = temp
+            temp = after
+
 my_linked_list = LinkedList(11)
 my_linked_list.append(3)
 my_linked_list.append(5)
 my_linked_list.append(6)
 my_linked_list.print_list()
-print("after removing")
-my_linked_list.remove(1)
+print("after reversing")
+my_linked_list.reverse()
 my_linked_list.print_list()
-
-
